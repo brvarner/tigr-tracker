82161e39fd237b05e8818f7ee5bfdf860454752f
Timple Basinger <tbasinger@protonmail.com>
2021-12-01 21:36:30 -0600

    updates 2

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -4,18 +4,49 @@ import db from '../db';
 const router = Router();
 
 //GET /api/users 
-router.get('/', async (req, res, next) => {
-    res.json({MSG: 'test blogs get'});
-    
-    
-    // try {
-    //     const users = await db.users.all()
-    //     res.json(users);
-    // } catch (error) {
-    //     console.log(error);
-    //     res.status(500).json({ msg: 'awww looks like your code sucks!', error });
-    //     //, error sends e to postman so can read it    
-    // }
+router.get('/:id?', async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        if (id) {
+            res.json({ id, msg: 'get one test' });
+        } else {
+            res.json({ id, msg: 'get all test' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'ya code sucks' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const newUser = { ...req.body };
+    try {
+        res.json({ msg: 'post test', userTest: newUser });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'ya code sucks' });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    const editUser = { ...req.body };
+    try {
+        res.json({ id, msg: 'put test', testUser: editUser });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'ya code sucks' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        res.json({ id, msg: 'delete test' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'ya code sucks' });
+    }
 });
 
 
