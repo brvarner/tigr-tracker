f8bc15a94e89c08ba09d7eb504d26160cc58e1bc
Nick <nwharr12@gmail.com>
2021-12-03 16:03:25 -0600

    admin page new stuff

--- src/client/components/admin.tsx
+++ src/client/components/admin.tsx
@@ -2,43 +2,60 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 
 const Admin = (props: AppProps) => {
-    const [greeting, setGreeting] = useState<string>('');
+    const [title, setTitle] = useState<string>('');
+    const [sku, setSku] = useState<string>('');
+    const [regPrice, setRegPrice] = useState<number>(0);
+    const [salePrice, setSalePrice] = useState<number>(0);
+    const [onSale, setOnSale] = useState<number>(0);
+    const [desc, setDesc] = useState<string>('');
 
-    useEffect(() => {
-        async function getGreeting() {
-            try {
-                const res = await fetch('/api/hello');
-                const greeting = await res.json();
-                setGreeting(greeting);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getGreeting();
-    }, []);
 
     return (
-        <form className="container my-5">
-            <h1 className="text-primary text-center">Hello {greeting}!</h1>
+        <div style={{ backgroundColor: '#8f5325'}}>
+            <form className="container my-5">
+                <h1 className="text-success text-center">Admin</h1>
 
-            <div className="form-group">
+                <div className="card rounded" >
+                    <div className="card-body">
 
-            </div>
-            <div className="input-group">
-                <label htmlFor="Product-Title">Product Title</label>
-                <input type="text" placeholder="Product Title" id="admin-product-title" className="admin-input-description form-control" />
-                <label htmlFor="Product-Title">SKU</label>
-                <input type="text" placeholder="SKU" className="admin-input-description form-control" />
-                <label htmlFor="Product-Title">Regular Price</label>
-                <input type="text" placeholder="Regular Price" className="admin-input-description form-control" />
-                <label htmlFor="Product-Title">Sale Price</label>
-                <input type="text" placeholder="Sale Price" className="admin-input-description form-control" />
-                <label htmlFor="Product-Title">On Sale?</label>
-                <input type="text" placeholder="On Sale?" className="admin-input-description form-control" />
-                <label htmlFor="Product-Title">Description</label>
-                <input type="text" placeholder="Description" className="admin-input-description form-control" />
-            </div>
-        </form>
+                        <div className="input-group d-flex flex-column">
+
+                            <div id="productTitle" className="mb-3">
+                                <label htmlFor="Product-Title">Product Title</label>
+                                <input type="text" placeholder="Product Title" id="admin-product-title" className="admin-input-description form-control" />
+                            </div>
+
+                            <div id="sku" className="mb-3">
+                                <label htmlFor="Product-Title">SKU</label>
+                                <input type="text" placeholder="SKU" className="admin-input-description form-control" />
+                            </div>
+
+                            <div id="regPrice" className="mb-3">
+                                <label htmlFor="Product-Title">Regular Price</label>
+                                <input type="text" placeholder="Regular Price" className="admin-input-description form-control" />
+                            </div>
+
+                            <div id="salePrice" className="mb-3">
+                                <label htmlFor="Product-Title">Sale Price</label>
+                                <input type="text" placeholder="Sale Price" className="admin-input-description form-control" />
+                            </div>
+
+                            <div id="onSale" className="mb-3">
+                                <label htmlFor="Product-Title">On Sale?</label>
+                                <input type="text" placeholder="On Sale?" className="admin-input-description form-control" />
+                            </div>
+
+                            <div id="desc" className="mb-3">
+                                <label htmlFor="Product-Title">Description</label>
+                                <input type="text" placeholder="Description" className="admin-input-description form-control" />
+                            </div>
+
+                            <button type="Submit" className="btn btn-primary btn-sm">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
