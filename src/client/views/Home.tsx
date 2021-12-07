d45e1ac615d194f10b69ed7796a929fe8454ed82
mmcorbin74 <mmcorbin74@gmail.com>
2021-12-07 16:34:26 -0600

    added Modal

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -7,6 +7,7 @@ import Hero from '../components/Hero';
 
 
 
+
 const Home: React.FC<HomeProps> = props => {
 
     return (
---
+++ src/client/views/Products.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Products = (props: ProductsProps) => {
+    const [products, setProducts] = useState([]);
+    
+
+    React.useEffect(() => {
+        (async () => {
+            const res = await fetch('/api/products');
+            if (res.ok) {
+                const products = await res.json();
+                setProducts(products);
+            }
+        })()
+    }, [])
+
+    
+    return (
+        <main className="container" >
+            <h1> Products </h1>
+            <section className="row justify-content-center mt-5">
+                {products.map(product => (
+                    <div className="col-2" key={`product-card-${product.id}`}>
+                        <div className="card shadow-lg my-2">
+                            <div className="card-body">
+                                <img src={product.image} alt="item image" className="card-img-top" />
+                                <h4 className="card-title my-2">{product.product_title}</h4>
+                                <p className="card-subtitle text-muted">{product.description}</p>
+                                <p className="card-text">Price: $ {product.regular_price}</p>
+                                <p className="card-text">Sale Price: $ {product.sale_price}</p>
+                                <a href="#" className="card-link btn btn-primary">Track</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </main>
+    )
+}
+
+interface ProductsProps { }
+
+export default Products
